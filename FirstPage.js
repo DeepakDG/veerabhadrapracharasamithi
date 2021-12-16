371bea2e5f0e083789ddfc3b27f1a3a8171d7cf8
DeepakWin10 <deepakganachari3@gmail.com>
2021-12-16 19:10:41 +0530

    removed typing text

--- FirstPage.js
+++ FirstPage.js
@@ -45,89 +45,7 @@ const actions = [
 ];
 
 class TypingText extends Component<{}> {
-  constructor() {
-    super();
 
-    this.index = 0;
-
-    this.typing_timer = -1;
-
-    this.blinking_cursor_timer = -1;
-
-    this.state = {text: '', blinking_cursor_color: 'transparent'};
-  }
-
-  componentDidMount() {
-    this.typingAnimation();
-    this.blinkingCursorAnimation();
-  }
-
-  componentWillUnmout() {
-    clearTimeout(this.typing_timer);
-
-    this.typing_timer = -1;
-
-    clearInterval(this.blinking_cursor_timer);
-
-    this.blinking_cursor_timer = -1;
-  }
-
-  typingAnimation = () => {
-    clearTimeout(this.typing_timer);
-
-    this.typing_timer = -1;
-
-    if (this.index < this.props.text.length) {
-      if (this.refs.animatedText) {
-        this.setState(
-          {text: this.state.text + this.props.text.charAt(this.index)},
-          () => {
-            this.index++;
-
-            this.typing_timer = setTimeout(() => {
-              this.typingAnimation();
-            }, this.props.typingAnimationDuration);
-          },
-        );
-      }
-    }
-  };
-
-  blinkingCursorAnimation = () => {
-    this.blinking_cursor_timer = setInterval(() => {
-      if (this.refs.animatedText) {
-        if (this.state.blinking_cursor_color == 'transparent')
-          this.setState({blinking_cursor_color: this.props.color});
-        else this.setState({blinking_cursor_color: 'transparent'});
-      }
-    }, this.props.blinkingCursorAnimationDuration);
-  };
-
-  render() {
-    return (
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-        }}>
-        <Text
-          ref="animatedText"
-          style={{
-            color: this.props.color,
-            fontSize: this.props.textSize,
-            textAlign: 'center',
-            marginTop: this.props.marginTop,
-            marginHorizontal: this.props.marginHorizontal,
-            fontFamily: this.props.fontFamily,
-          }}>
-          {this.state.text}
-
-          <Text style={{color: this.state.blinking_cursor_color}}>|</Text>
-        </Text>
-      </View>
-    );
-  }
 }
 
 const DATA = [
@@ -341,7 +259,6 @@ export default class FirstPage extends Component<{}> {
     );
     return (
       <View style={styles.MainContainer}>
-        <TypingText text={'With'} />
         <FlatList
           data={DATA}
           renderItem={renderItem}
