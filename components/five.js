27aef8ad923db104bf272a9102a086fcd54954ac
DeepakWin10 <deepakganachari3@gmail.com>
2022-01-23 10:05:23 +0530

    toolbar header added

--- components/five.js
+++ components/five.js
@@ -1,10 +1,142 @@
-import React from "react";
-import { StyleSheet, View, Text, ScrollView } from "react-native";
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Alert,
+  ImageBackground,
+  Animated,
+  Button,
+  ScrollView,
+} from "react-native";
+import PropTypes from "prop-types";
 import PagerView from "react-native-pager-view";
 
-const MyPager = () => {
-  return (
-    <View style={{ flex: 1 }}>
+class TypingText extends Component<{}> {
+  constructor() {
+    super();
+
+    this.index = 0;
+
+    this.typing_timer = -1;
+
+    this.blinking_cursor_timer = -1;
+
+    this.state = { text: "", blinking_cursor_color: "transparent" };
+  }
+
+  componentDidMount() {
+    this.typingAnimation();
+    this.blinkingCursorAnimation();
+  }
+
+  componentWillUnmout() {
+    clearTimeout(this.typing_timer);
+
+    this.typing_timer = -1;
+
+    clearInterval(this.blinking_cursor_timer);
+
+    this.blinking_cursor_timer = -1;
+  }
+
+  typingAnimation = () => {
+    clearTimeout(this.typing_timer);
+
+    this.typing_timer = -1;
+
+    if (this.index < this.props.text.length) {
+      if (this.refs.animatedText) {
+        this.setState(
+          { text: this.state.text + this.props.text.charAt(this.index) },
+          () => {
+            this.index++;
+
+            this.typing_timer = setTimeout(() => {
+              this.typingAnimation();
+            }, this.props.typingAnimationDuration);
+          }
+        );
+      }
+    }
+  };
+
+  blinkingCursorAnimation = () => {
+    this.blinking_cursor_timer = setInterval(() => {
+      if (this.refs.animatedText) {
+        if (this.state.blinking_cursor_color == "transparent")
+          this.setState({ blinking_cursor_color: this.props.color });
+        else this.setState({ blinking_cursor_color: "transparent" });
+      }
+    }, this.props.blinkingCursorAnimationDuration);
+  };
+
+  render() {
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "flex-start",
+        }}
+      >
+        <Text
+          ref="animatedText"
+          style={{
+            color: this.props.color,
+            fontSize: this.props.textSize,
+            textAlign: "center",
+            marginTop: this.props.marginTop,
+            marginHorizontal: this.props.marginHorizontal,
+            fontFamily: this.props.fontFamily,
+          }}
+        >
+          {this.state.text}
+
+          <Text style={{ color: this.state.blinking_cursor_color }}>|</Text>
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default class FamilyInfo extends Component<{}> {
+  constructor() {
+    super();
+    this.state = {
+      isVisible: true,
+      ticketIndex: 1,
+      startValue: new Animated.Value(0),
+      endValue: 1,
+      duration: 10000,
+    };
+  }
+
+  componentDidMount() {
+    var that = this;
+  }
+  static navigationOptions = {
+    // header: null
+    title: "Family-Info",
+    headerTintColor: '#FFDF00',
+    headerStyle: {
+      backgroundColor: '#bb1471',
+      borderBottomColor: '#FFDF00',
+      borderBottomWidth: 3,
+    },
+    headerTitleStyle: {
+      fontSize: 18,
+    },
+    //Sets Header text of Status Bar
+  };
+
+  render() {
+    const ticketHeight = 400;
+    const { navigate } = this.props.navigation;
+    return (
+        <View style={{ flex: 1 }}>
       <PagerView style={styles.viewPager} initialPage={0}>
         <View style={styles.page} key="1">
           <ScrollView>
@@ -67,26 +199,58 @@ const MyPager = () => {
         </View>
       </PagerView>
     </View>
-  );
-};
-
+    );
+  }
+}
 const styles = StyleSheet.create({
-  viewPager: {
+  MainContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    paddingTop: Platform.OS === "ios" ? 20 : 0,
+  },
+
+  image: {
+    flex: 1,
+  },
+   viewPager: {
     flex: 1,
     marginTop: 0,
-    backgroundColor: "#fe724c",
+    backgroundColor: '#fe724c',
     padding: 15,
   },
   page: {
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   textalign: {
-    textAlign: "justify",
+    textAlign: 'justify',
     fontSize: 20,
-    color: "#ffffff",
+    color: '#ffffff',
     padding: 15,
   },
 });
 
-export default MyPager;
+TypingText.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  marginTop: PropTypes.number,
+  marginHorizontal: PropTypes.number,
+  textSize: PropTypes.number,
+  fontFamily: PropTypes.fontFamily,
+  typingAnimationDuration: PropTypes.number,
+  blinkingCursorAnimationDuration: PropTypes.number,
+};
+
+TypingText.defaultProps = {
+  text: "Default Typing Animated Text.",
+  color: "rgb(255,223,0)",
+  marginTop: 30,
+  marginHorizontal: 30,
+  textSize: 18,
+  fontFamily: "Courgette.Regular",
+  typingAnimationDuration: 5,
+  blinkingCursorAnimationDuration: 450,
+};
